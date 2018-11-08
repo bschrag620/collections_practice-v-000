d3d704a03c8c9a9c0247b7fb5c231e86e2588a92
Brad Schrag <brad.schrag@gmail.com>
2018-11-08 13:43:09 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,5 +41,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|word, index| if index != 1 word << "s"}
+  array.each_with_index.collect {|word, index| word << "s" if index != 1}
 end
